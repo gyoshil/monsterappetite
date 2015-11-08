9c393488163d0a16e6852ea8ad8ff2b3a7f13f66
Mark Santolucito <mark.santolucito@yale.edu>
2015-11-08 14:34:39 -0500

    get the params from qualitrics

--- client/snackazon.js
+++ client/snackazon.js
@@ -1,5 +1,9 @@
-Router.route('/snackazon',{
-  template: 'snackazon'
+var group;
+
+Router.route('/snackazon', function () {
+  this.render('snackazon');
+  group = this.params.query.group;
+  console.log(group);
 });
 
 Router.configure({
@@ -8,5 +12,6 @@ Router.configure({
 
 Template.snackazon.barImg = function () {
   // only show lobby if we're not in a game
-  return "https://pixabay.com/static/uploads/photo/2014/04/02/10/18/granola-303384_640.png";
+  //return "https://pixabay.com/static/uploads/photo/2014/04/02/10/18/granola-303384_640.png";
+  return group;
 };
