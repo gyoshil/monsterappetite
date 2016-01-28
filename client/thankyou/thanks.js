c525bd28ddbef08d19b21178b7bd8ed3a85699de
mlhwang <mlh2169@tc.columbia.edu>
2016-01-27 21:14:15 -0500

    updates

--- client/thankyou/thanks.js
+++ client/thankyou/thanks.js
@@ -12,12 +12,3 @@ $(document).ready(function(){
 });
 
 
-/*function codeAddress() {  
-            var cron = require('cron');
-			var cronJob = cron.job("*/5 * * * * *", function(){
-    			// perform operation e.g. GET request http.get() etc.
-    			console.info('cron job completed');
-			}); 
-			cronJob.start();
-        }
-        window.onload = codeAddress;*/
