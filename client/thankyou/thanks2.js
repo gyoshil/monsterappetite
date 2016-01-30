816e02b52fde78a97671cea2f602d1b3f820d97c
mlhwang <mlh2169@tc.columbia.edu>
2016-01-29 22:29:43 -0500

    thanks2 has been made for session 2

---
+++ client/thankyou/thanks2.js
@@ -0,0 +1,14 @@
+
+Router.route('/thanks',{
+  template: 'thanks'
+});
+
+Router.configure({
+    layoutTemplate: 'main'
+});
+
+$(document).ready(function(){    
+    $("[data-toggle=tooltip]").tooltip();
+});
+
+
