430de4c2a1618b73af017bf18f5dd72b62d99292
Mark Santolucito <mark.santolucito@yale.edu>
2016-02-07 12:16:57 +0900

    use localhost smtp

--- server/smtp.js
+++ server/smtp.js
@@ -1,3 +1,3 @@
-Meteor.startup(function () {
-process.env.MAIL_URL = 'smtp://mhwang502:gats346by@smtp.sendgrid.net:587';
-});
+//Meteor.startup(function () {
+//process.env.MAIL_URL = 'smtp://mhwang502:PASSWORD@smtp.sendgrid.net:587';
+//});
