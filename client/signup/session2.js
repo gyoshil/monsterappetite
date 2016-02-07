e0d4b8eb5095ed20f9111bd027b6914b1492d90d
mlhwang <mlh2169@tc.columbia.edu>
2016-02-07 18:43:52 +0900

    added instruction pages - all done

---
+++ client/signup/session2.js
@@ -0,0 +1,7 @@
+Router.route('/session2',{
+  template: 'session2'
+});
+
+Router.configure({
+    layoutTemplate: 'main'
+});
---
+++ client/snackazon/intro_session2.js
@@ -0,0 +1,8 @@
+
+Router.route('/snackazon/intro_session2', function () {
+  this.render('intro_session2');
+});
+
+Router.configure({
+    layoutTemplate: 'main'
+});
