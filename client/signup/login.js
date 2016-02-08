745587384c8f8518be47aa26129f84fd101c0796
mlhwang <mlh2169@tc.columbia.edu>
2016-02-08 19:09:31 +0900

    updating session 2 links

--- client/signup/login.js
+++ client/signup/login.js
@@ -1,5 +1,5 @@
 Router.route('/login/:player_id', function () {
   var player_id = this.params.player_id;
   document.cookie="u_id="+player_id+"; path=/";
-  Router.go("game")
+  Router.go("/snackazon/intro_session2")
 });
