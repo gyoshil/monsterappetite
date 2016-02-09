0aea2597f16352b6cda273eb668c3eb8dfe48df2
mlhwang <mlh2169@tc.columbia.edu>
2016-02-09 23:46:44 +0900

    update

--- client/pregame/pregame.js
+++ client/pregame/pregame.js
@@ -5,3 +5,17 @@ Router.route('/pregame',{
 Router.configure({
     layoutTemplate: 'main'
 });
+
+
+Template.pregame.helpers({
+	condition: function () {
+		console.log (player());
+	    if (player().group == "gain")  {
+	    	return '/pregame_g'
+	    }
+	    else {
+	    	return '/pregame_l'
+	    }
+	    return "error_no_group"
+	}
+});
