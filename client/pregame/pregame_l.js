9c9bf94cbf15ada7866188b428112ac0f8d3ec94
mlhwang <mlh2169@tc.columbia.edu>
2016-02-10 10:12:05 +0900

    instruction page

--- client/pregame/pregame_l.js
+++ client/pregame/pregame_l.js
@@ -6,9 +6,9 @@ Router.configure({
     layoutTemplate: 'main'
 });
 
-Template.instruction.events ({
+/*Template.instruction.events ({
   'click .done': function(event,template) {
     window.scrollTo(0,0)
 	}
-})
+})*/
 
