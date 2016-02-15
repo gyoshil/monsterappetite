2b3a896134ab2058aa18ab31a2ffc3ff10097052
mlhwang <mlh2169@tc.columbia.edu>
2016-02-15 12:43:51 +0900

    buttons all fixed so user can click anywhre in the button. + Session2 intro page is where Session 2 starts as opposed to Snackazon

--- client/signup/login.js
+++ client/signup/login.js
@@ -1,7 +1,7 @@
 Router.route('/login/:player_id', function () {
   var player_id = this.params.player_id;
   document.cookie="u_id="+player_id+"; path=/";
-  Router.go("/snackazon/intro_session2")
+  Router.go("/session2")
 });
 
 Router.route('/login', function () {
