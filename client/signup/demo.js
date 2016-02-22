f1edd0a4e8c8f76f0e4323290c9b0db77014b71c
mlhwang <mlh2169@tc.columbia.edu>
2016-02-22 22:43:12 +0900

    making a demo page that redirects to game with a user id and name

--- client/signup/demo.js
+++ client/signup/demo.js
@@ -1,5 +1,5 @@
 Router.route('/demo/:player_id', function () {
   var player_id = this.params.player_id;
   document.cookie="u_id="+player_id+"; path=/";
-  Router.go("/snackazon/game")
+  Router.go("/game")
 });
