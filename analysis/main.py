2658644d985b1b193996a51fce0626016bcff7ff
Mark Santolucito <msantolucuito13@gmail.com>
2016-03-24 20:01:47 +0900

    more precesoing

--- analysis/main.py
+++ analysis/main.py
@@ -73,19 +73,72 @@ query_dbs.tagCSVPlayers("PDQ3.csv",all,"calorie_influence_pre2",calorie_influenc
 query_dbs.tagCSVPlayers("PDQ3.csv",all,"calorie_influence_post2",calorie_influence);
 
 ########
-# PDQ
+# DQ
 #######
-def calorie_influence(mongoP,qualtricsP):
+def dq(q_num,mongoP,qualtricsP):
   if(qualtricsP=="NONE"):
     return "empty"
-  x = qualtricsP[18]
-  if(x==""): x=0
+  x = qualtricsP[q_num]
   return x
 
-query_dbs.tagCSVPlayers("PDQ1.csv",all,"calorie_influence_pre1",calorie_influence)
-query_dbs.tagCSVPlayers("PDQ2.csv",all,"calorie_influence_post1",calorie_influence)
-query_dbs.tagCSVPlayers("PDQ3.csv",all,"calorie_influence_pre2",calorie_influence)
-query_dbs.tagCSVPlayers("PDQ3.csv",all,"calorie_influence_post2",calorie_influence);
+def dq2(m,q) : return dq(3,m,q)
+def dq3(m,q) : return dq(4,m,q)
+def dq4(m,q) : return dq(5,m,q)
+def dq5(m,q) : return dq(6,m,q)
+def dq6(m,q) : return dq(7,m,q)
+def dq7(m,q) : return dq(8,m,q)
+def dq8(m,q) : return dq(9,m,q)
+
+query_dbs.tagCSVPlayers("DQ.csv",all,"dq2",dq2)
+query_dbs.tagCSVPlayers("DQ.csv",all,"dq3",dq3)
+query_dbs.tagCSVPlayers("DQ.csv",all,"dq4",dq4)
+query_dbs.tagCSVPlayers("DQ.csv",all,"dq5",dq5)
+query_dbs.tagCSVPlayers("DQ.csv",all,"dq6",dq6)
+query_dbs.tagCSVPlayers("DQ.csv",all,"dq7",dq7)
+query_dbs.tagCSVPlayers("DQ.csv",all,"dq8",dq8)
+
+
+########
+# FFQ 1st
+#######
+def ffq1_risk(mongoP,qualtricsP):
+  if(qualtricsP=="NONE"):
+    return "empty"
+  good_answers = [(2,1),(2,2),
+                  (3,1),(3,2),
+                  (12,1),(12,2),(12,3),
+                  (13,1),(13,2),(13,3),
+                  (14,1),(14,2),(14,3)]
+  if(csv_answer_check(mongoP,qualtricsP,good_answers)):
+    return "high"
+  return "low"
+
+query_dbs.tagCSVPlayers("FFQ1.csv",all,"ffq1_risk",ffq1_risk)
+
+
+
+########
+# FFQ 2nd
+#######
+def ffq2_score(mongoP,qualtricsP):
+  if(qualtricsP=="NONE"):
+    return "empty"
+  score = 0
+  completed = 23 #(22 now tho...)
+  def check2(q) :
+    if (qualtricsP[q]==2): score+=1
+  def check1(q) :
+    if (qualtricsP[q]==1): score+=1
+  #healthy answer is 2
+  healthy2 = [4,9,16,20,24]
+  #healthy answer is 1
+  healthy1 = [2,3,5,6,7,8,10,11,13,14,15,17,18,19,21,23]
+  #no 12 and 22 (see attention_pass)
+  map(check2,healthy2)
+  map(check1,healthy1)
+  return score*(23/completed)
+
+query_dbs.tagCSVPlayers("FFQ2.csv",all,"ffq2_score",ffq2_score)
 
 
 ########
@@ -151,7 +204,7 @@ def collapseOnePlayer (mongoP):
 def average(l):
   return sum(l)/len(l)
 
-x = list(map(collapseOnePlayer,query_dbs.mongoPlayers))
+#x = list(map(collapseOnePlayer,query_dbs.mongoPlayers))
 
 #pprint (x)
 
@@ -184,7 +237,37 @@ for (round_num,round_avg_score) in enumerate(average_scores):
 pprint (average_scores)
 '''
 
+
+######
+# FILTER ATTENTION
+######
+def attention_measure1(mongoP,qualtricsP):
+  if(qualtricsP=="NONE"):
+    return True
+  good_answers = [(9,4)]
+  if(csv_answer_check(mongoP,qualtricsP,good_answers)):
+    return True
+  return False
+
+def attention_measure2(mongoP,qualtricsP):
+  if(qualtricsP=="NONE"):
+    return True
+  good_answer1 = [(12,4)]
+  good_answer2 = [(22,3)]
+  if(csv_answer_check(mongoP,qualtricsP,good_answer1) and
+     csv_answer_check(mongoP,qualtricsP,good_answer2)):
+    return True
+  return False
+
+query_dbs.tagCSVPlayers("FFQ1.csv",all,"attention_measure1",attention_measure1)
+query_dbs.tagCSVPlayers("FFQ2.csv",all,"attention_measure2",attention_measure2)
+
+def attention_pass(p) :
+  return (p['attention_measure2'] and p['attention_measure1'])
+
+filtered_all = {k:v for (k,v) in all.items() if attention_pass(v)}
+
 ######
 # PRINT
 ######
-#csv_printer.printAll(all)
+csv_printer.printAll(filtered_all)
