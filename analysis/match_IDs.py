a113524dc8a943346a02da7e8eb3c538d4d6e808
mlhwang <mlh2169@tc.columbia.edu>
2016-11-20 14:12:02 -0500

    will add all new files

---
+++ analysis/match_IDs.py
@@ -0,0 +1,409 @@
+def match_IDs(string):
+    print "I have match the string '%s'" % string
+
+'''lists = [
+    ["hello", "world", "how", "grown", "you", "today", "?"],
+    ["growl", "is", "a", "now", "on", "appstore", "too bad"],
+    ["I", "wish", "I", "grow", "Magnum", "mustache", "don't you?"],
+]'''
+
+a =[	yqvi6AycizdfMW67J, 
+		Ka7uwDPR6rD8fmrYq, 
+		NvQHkQfCNThsLfjfy, 
+		naXyBAkcHQnvmWAq2, 
+		Qqh6muXhTuf3GrNiT, 
+		EKWRiN76o86oB7anS, 
+		XEFQSfMA4Np9cuPw7, 
+		no24mX6RndnhrbK2G, 
+		oWHGBei3dY4SjyLpw, 
+		tDQkgwFiLneeqykeP, 
+		wGaSKjvm4o49LhLrg, 
+		A8Cj2fKdNtZJqYLwh, 
+		JSbt6gZfMo7joBzGv, 
+		w7iHdtHuvS62tpX9A, 
+		e3qn8csAYTbHfMxkK, 
+		SJfYbZhnHTBay5yyX, 
+		oD7a6GFkarvp84PRt, 
+		veJrsxDb3LXrES4xG, 
+		P3KQ9YkjZxt4RLM8n, 
+		yZoeMgzGtaMeqaFnP, 
+		bvQKD5bRjLyvwCpPy, 
+		qXjSPCjisfJ95eEvt, 
+		kzsfFDBTiA8Hm5zG8, 
+		xPohAATJY45H5Nmhy, 
+		5eAZsM6sSPgLp2AGc, 
+		DW37WiLZena9WDYfN, 
+		sffrXtuXr5TiAB4mp, 
+		eBK9ZbEMsYZr4qvN5, 
+		J4zEKsDmhBZRemQxx, 
+		Af6EwezJnoFReEpua, 
+		7WepedTffMwj4827y, 
+		NEBKBc4v4z22JjHsM, 
+		WWN5DFC5NTCBf5mzm, 
+		uSMjBeZWWWbr389eX, 
+		2EaSSMTDRTavgzxWY, 
+		uaKgcq3eKvpoHhiRp, 
+		u469QjCESYfQpGHJC, 
+		nTjB9cbr4ip2uXa8g, 
+		Lcmknpky6pFHN3tQu, 
+		pnco6QuR9SNdHu77x, 
+		ZNHoLFfThXn3dxpiE, 
+		XXfq2mbXgiwoevAzb, 
+		CLtNCJghz2jZqxeK9, 
+		Yq2tfTdFxrtJJrtiK, 
+		MLNEv85tZLCBBPkBJ, 
+		HzyJf3ecpepyFffL9, 
+		zBGtTaoT9NXXoiDCo, 
+		e39vqG226v3BajR5r, 
+		hFMehFgYKLkcpyuQ9, 
+		sXZRJJZZ4ukn5Rhsa, 
+		KhryLZQ5TvoZre7Wq, 
+		Bpj4sGSnkFdkCCxNf, 
+		pJmeMQeWgG5t6W9qP, 
+		qBC3M3KQn9vNCcPqE, 
+		eCXK2ArfS3criAJTB, 
+		ufYSbrs2puEYHomhr, 
+		5FeAYE9ht2bBCK8Lk, 
+		E3NcHz7W573rHYeyY, 
+		GBR5Ti7uHcvswPGew, 
+		47GQnCY9tKY6QcziM, 
+		4KFh6N3nNwuJAz9bM, 
+		L4ym7sk99zbs7SpRS, 
+		iNRN7ctrXkwmZQSxt, 
+		XCDm5KKWWLKo82ig8, 
+		nyhJYddWvp3BB3QQw, 
+		Wxu8k7df9yj4j6573, 
+		Zni6Soax4z3GW8Df2, 
+		z5RvEc5BoDxBiH3iy, 
+		AqpMTK8pfa3ZDgCnX, 
+		DE4yEJwaXQHEzKcQa, 
+		gouSf322Wfbff39Ry, 
+		zr6MbzW9igA4q3Bpk, 
+		peZip3nZ25pQ4ism4, 
+		bWNtxPkGd8xNqDf2H, 
+		Xyy2u4fX5w8ZXBK3y, 
+		RRGteZxa2sZLjy7tF, 
+		nxAYw9WDEpZEActyW, 
+		FK4Hu9RZqcpEHNEsT, 
+		JzmhwhLyzer2RAe6v, 
+		LpifnQpyoAC96S573, 
+		KtmBDbHyRWmxF3pSe, 
+		Go273GW8uicZrCdSD, 
+		BNHYtgF3y6oE5Z7KQ, 
+		kErKZoNcgLDEQLh9h, 
+		Wsfum3agWhXQX8CXx, 
+		cLfpHqBMQyBqJmqZN, 
+		CpCwPvRPuXbcP6cz2, 
+		79cjqjh3rhGT85Z35, 
+		r7ouGiTYscdMz7S96, 
+		ozofPi8aj5tyaT4eg, 
+		SPtAAc7NJbzx6HGnv, 
+		btruDZRTdLHpSMw8Z, 
+		q7T5qjCJmLKiFDM7B, 
+		udy3MprwBQ5HiyMCT, 
+		AwhQeCLDxEyvjTy3X, 
+		wZbv3M3dFcDzAi36e, 
+		yqM2BRFq8ZRfjjLkq, 
+		ppT2KoixinaKc9zLD, 
+		2uka6dm2y2BiZJ2zv, 
+		46jJ5bQRXvenhFmTb, 
+		JYCXAYdtPkWscrdLB, 
+		KM8aPtzLYKjy9Wd6r, 
+		PqWLTxemD9c69esfF, 
+		RiaXcXZD8EMe3D675, 
+		8AFCu9YsQmR2WDkrk, 
+		CBqTRZWRzXAgfN9Yy, 
+		2HF6FqEczphrX2Bid, 
+		J2cummHqPe6hMCzQa, 
+		wLGnCDxPy8oExZXAx, 
+		DJkv5Fv9S4kWNyB3h, 
+		Ny9gao85Xj9EsukBG, 
+		8rkTWSowd8QnKYkrE, 
+		KgZMYh23LzaJQn9xQ, 
+		ANpFmJSXtcMZLsKb7, 
+		TRHpDyjtFJ4Bf98dL, 
+		YGJd65DmqYzQazNgB, 
+		BynvkWSwo2taRoXrX, 
+		nXMXFzpgTi4qgwWK6, 
+		cSZYtAZP8JYGj5YJm, 
+		7Rn59QWiRLMAcySTj, 
+		rzgwdpdiJDSxYGZnA, 
+		iw4jvD5va5fnWukXC, 
+		TYLSC3JKidf2ijqLa, 
+		65rkkh9mvCWJswXdW, 
+		F3ZWcZX7ARkhPJmRW, 
+		zHSLFZ2ThvDD5HYzx, 
+		RMo96PZASrs7J7ShD, 
+		XsvK4NT3Nh5wjR8En, 
+		c7KXKPxqoFftvbiRo, 
+		k3GpWCr7PFT6C8hp7, 
+		9ywHQR3mBPMwFxoz9, 
+		7pCqtPS3WupS8an42, 
+		YiHmJ4qLbxiwyjMFd, 
+		trErkp8jGhzNAdEsC, 
+		i8NjyEXvuMDxg5nGL, 
+		K3ZMCWFyEZDHrTyJs, 
+	]
+
+
+b = [
+		i7HMczRwQwWbacY4R	,	2,15,2016, 5:50:00, AM
+		DW37WiLZena9WDYfN	,	2,15,2016, 8:59:00, AM
+		A8Cj2fKdNtZJqYLwh	,	2,15,2016, 9:38:00, AM
+		F3ZWcZX7ARkhPJmRW	,	2,15,2016, 1:51:00, PM
+		9bSZGxpzwYt8Zz8Kx	,	2,15,2016, 2:47:00, PM
+		peZip3nZ25pQ4ism4	,	2,15,2016, 3:28:00, PM
+		hFMehFgYKLkcpyuQ9	,	2,15,2016, 4:23:00, PM
+		GL2hudNdtRviGrbv6	,	2,15,2016, 4:42:00, PM
+		YiHmJ4qLbxiwyjMFd	,	2,15,2016, 4:52:00, PM
+		Xyy2u4fX5w8ZXBK3y	,	2,15,2016, 5:05:00, PM
+		PxZYxaGdJhcnoWWsT	,	2,15,2016, 5:34:00, PM
+		qHfPbdZH5ooEhRGj5	,	2,15,2016, 9:08:00, PM
+		nXMXFzpgTi4qgwWK6	,	2,15,2016, 9:08:00, PM
+		idBwv2cHpmaynrZhD	,	2,15,2016, 9:10:00, PM
+		iw4jvD5va5fnWukXC	,	2,15,2016, 9:42:00, PM
+		bvQKD5bRjLyvwCpPy	,	2,15,2016, 11:04:00, PM
+		nTjB9cbr4ip2uXa8g	,	2,16,2016, 11:10:00, AM
+		ufYSbrs2puEYHomhr	,	2,16,2016, 1:27:00, PM
+		iNRN7ctrXkwmZQSxt	,	2,16,2016, 2:35:00, PM
+		i8NjyEXvuMDxg5nGL	,	2,16,2016, 2:59:00, PM
+		qf7G2up2hE5qScuK9	,	2,16,2016, 3:00:00, PM
+		Wxu8k7df9yj4j6573	,	2,16,2016, 5:46:00, PM
+		8rkTWSowd8QnKYkrE	,	2,16,2016, 7:12:00, PM
+		m3PpL6WcoykRyE6pm	,	2,16,2016, 9:12:00, PM
+		c7KXKPxqoFftvbiRo	,	2,17,2016, 11:40:00, AM
+		nyhJYddWvp3BB3QQw	,	2,17,2016, 6:26:00, PM
+		Go273GW8uicZrCdSD	,	2,17,2016, 8:50:00, PM
+		WxM5cQu2SaeRc9e8S	,	2,18,2016, 12:34:00, AM
+		Yq2tfTdFxrtJJrtiK	,	2,18,2016, 10:56:00, AM
+		nn9r35Fnm3dt4Mwac	,	2,18,2016, 11:55:00, AM
+		MxvzFzxhex6KtCcJv	,	2,18,2016, 12:11:00, PM
+		B2ZCZF78M52DaYdRe	,	2,18,2016, 2:59:00, PM
+		qBC3M3KQn9vNCcPqE	,	2,18,2016, 4:00:00, PM
+		q4HPxfuFegecSLdDp	,	2,18,2016, 4:24:00, PM
+		yZoeMgzGtaMeqaFnP	,	2,18,2016, 5:13:00, PM
+		R7dYKeoHCd6LvxxqK	,	2,18,2016, 6:13:00, PM
+		rwmxw6YBuepteyj3w	,	2,18,2016, 7:55:00, PM
+		2EaSSMTDRTavgzxWY	,	2,18,2016, 8:01:00, PM
+		79cjqjh3rhGT85Z35	,	2,20,2016, 8:35:00, PM
+		wtSLbuDzbcFjvfDEf	,	2,21,2016, 1:24:00, AM
+		tyLjuwcbAbuazEQiG	,	2,21,2016, 4:37:00, AM
+		u469QjCESYfQpGHJC	,	2,21,2016, 9:40:00, AM
+		SoKJoq72nqsZeYAav	,	2,21,2016, 9:58:00, AM
+		EsvySo9AtybDSYAs8	,	2,21,2016, 10:58:00, AM
+		JRcAvo7ghDwhDZL84	,	2,22,2016, 11:58:00, AM
+		MrGZ6mh63fudmqYDf	,	2,22,2016, 3:40:00, PM
+		Lcmknpky6pFHN3tQu	,	2,23,2016, 8:52:00, AM
+		5bASBojyYxYwsQXxd	,	2,23,2016, 2:18:00, PM
+		YtSdEqNJzaQeCopij	,	2,23,2016, 2:20:00, PM
+		6SEWxX7BWwdBNKtgP	,	2,23,2016, 2:36:00, PM
+		AqpMTK8pfa3ZDgCnX	,	2,23,2016, 3:02:00, PM
+		CLtNCJghz2jZqxeK9	,	2,23,2016, 3:07:00, PM
+		MyrQHPY3F4WLXmc5X	,	2,23,2016, 3:18:00, PM
+		z3zTMtRhkLDei8wuP	,	2,24,2016, 10:58:00, AM
+		YGJd65DmqYzQazNgB	,	2,24,2016, 7:11:00, PM
+		fhuLXXHqMQ5d8rDAn	,	2,25,2016, 2:11:00, PM
+		QmvwaSmSW9vegKCLm	,	2,26,2016, 2:58:00, AM
+		u4ij2RsfoD2QLvBEb	,	2,26,2016, 2:02:00, PM
+		AwhQeCLDxEyvjTy3X	,	2,27,2016, 10:19:00, PM
+		43rAun7MweDZDbiR5	,	2,28,2016, 2:48:00, AM
+		YM8venxunpNq9B2xq	,	2,28,2016, 4:16:00, AM
+		no24mX6RndnhrbK2G	,	2,28,2016, 1:35:00, PM
+		bgy5jAMHTgznc4ux5	,	2,28,2016, 1:57:00, PM
+		Bpj4sGSnkFdkCCxNf	,	2,28,2016, 3:03:00, PM
+		ptvCG2FEvcGE6NwA6	,	2,29,2016, 1:44:00, AM
+		mMzbDo23w5dTrdSEk	,	2,29,2016, 8:43:00, AM
+		b3JmtzQJQgAcnZnYE	,	2,29,2016, 9:40:00, AM
+		6A43r9ZERAEEDbZ8E	,	2,29,2016, 9:43:00, AM
+		XAgek4Txgcr6zNhkX	,	2,29,2016, 9:53:00, AM
+		dBkvJWWnppXkwvRnw	,	2,29,2016, 11:39:00, AM
+		2HF6FqEczphrX2Bid	,	2,29,2016, 1:23:00, PM
+		XgZraYjGuDarzP3r7	,	2,29,2016, 6:07:00, PM
+		PqWLTxemD9c69esfF	,	2,29,2016, 6:27:00, PM
+		za7LyLyEjai9LEXHc	,	3,1,2016, 1:42:00, AM
+		PmfEWQEeSM8RQm9sL	,	3,1,2016, 4:28:00, AM
+		wZbv3M3dFcDzAi36e	,	3,1,2016, 5:46:00, AM
+		LpifnQpyoAC96S573	,	3,1,2016, 9:35:00, AM
+		pnco6QuR9SNdHu77x	,	3,1,2016, 10:07:00, AM
+		NLM74X7BawyDFny4v	,	3,1,2016, 11:16:00, AM
+		DyGdnuktRvCnzAwJi	,	3,1,2016, 11:37:00, AM
+		xnnYT3aWf6eE4Q6y9	,	3,1,2016, 12:01:00, PM
+		XCDm5KKWWLKo82ig8	,	3,1,2016, 12:19:00, PM
+		usYEPKufrajuNF47i	,	3,1,2016, 1:26:00, PM
+		K3ZMCWFyEZDHrTyJs	,	3,1,2016, 3:47:00, PM
+		8RWtRBznZwaQXtTjy	,	3,1,2016, 5:32:00, PM
+		9n5EpvJB2WfCAocg2	,	3,1,2016, 5:35:00, PM
+		7pCqtPS3WupS8an42	,	3,1,2016, 6:18:00, PM
+		HzyJf3ecpepyFffL9	,	3,1,2016, 6:58:00, PM
+		TYLSC3JKidf2ijqLa	,	3,1,2016, 11:25:00, PM
+		RMo96PZASrs7J7ShD	,	3,2,2016, 4:56:00, AM
+		zr6MbzW9igA4q3Bpk	,	3,2,2016, 6:30:00, AM
+		8AFCu9YsQmR2WDkrk	,	3,2,2016, 10:23:00, AM
+		FK4Hu9RZqcpEHNEsT	,	3,2,2016, 3:01:00, PM
+		kzsfFDBTiA8Hm5zG8	,	3,2,2016, 8:17:00, PM
+		zBGtTaoT9NXXoiDCo	,	3,2,2016, 10:59:00, PM
+		5FeAYE9ht2bBCK8Lk	,	3,4,2016, 1:47:00, AM
+		ppT2KoixinaKc9zLD	,	3,4,2016, 1:56:00, AM
+		J2cummHqPe6hMCzQa	,	3,4,2016, 8:23:00, AM
+		TmShwF9bwp277DCmb	,	3,4,2016, 12:42:00, PM
+		XXfq2mbXgiwoevAzb	,	3,5,2016, 7:53:00, AM
+		dnjt4zkQjoZRiipyw	,	3,5,2016, 9:43:00, AM
+		A4p7aBR98rJFBJaEL	,	3,5,2016, 9:50:00, AM
+		BefdwBcYMvaHfGgwn	,	3,5,2016, 9:56:00, AM
+		oD7a6GFkarvp84PRt	,	3,5,2016, 10:40:00, AM
+		trErkp8jGhzNAdEsC	,	3,5,2016, 11:16:00, PM
+		fQW4FvoD7YJtY65Xj	,	3,6,2016, 9:11:00, AM
+		XjQr98bXFF7M3ndcy	,	3,6,2016, 9:53:00, AM
+		5TdrRvXeviEEB2782	,	3,6,2016, 9:58:00, AM
+		ozofPi8aj5tyaT4eg	,	3,6,2016, 10:18:00, AM
+		gouSf322Wfbff39Ry	,	3,6,2016, 10:47:00, AM
+		zHSLFZ2ThvDD5HYzx	,	3,6,2016, 11:14:00, AM
+		nxAYw9WDEpZEActyW	,	3,6,2016, 1:45:00, PM
+		sRnsZm6AXGfuFXaBL	,	3,6,2016, 2:08:00, PM
+		EK5MKqzgwbKp4H3b3	,	3,6,2016, 7:06:00, PM
+		qgdrKE9jGNxfQQS8Z	,	3,6,2016, 9:26:00, PM
+		7WepedTffMwj4827y	,	3,6,2016, 9:29:00, PM
+		qXjSPCjisfJ95eEvt	,	3,6,2016, 10:01:00, PM
+		rzgwdpdiJDSxYGZnA	,	3,7,2016, 12:17:00, AM
+		J4zEKsDmhBZRemQxx	,	3,7,2016, 8:08:00, AM
+		L4YKzb3s57NWd6NAv	,	3,7,2016, 8:17:00, AM
+		H4f5GuygEFHZk35Nx	,	3,7,2016, 8:56:00, AM
+		3Scv4Lh7AfesJPwhd	,	3,7,2016, 11:07:00, AM
+		eBK9ZbEMsYZr4qvN5	,	3,7,2016, 12:29:00, PM
+		3JMeDjgsrTwoAgacu	,	3,7,2016, 12:53:00, PM
+		KM8aPtzLYKjy9Wd6r	,	3,7,2016, 2:54:00, PM
+		BynvkWSwo2taRoXrX	,	3,7,2016, 3:27:00, PM
+		Wsfum3agWhXQX8CXx	,	3,7,2016, 5:39:00, PM
+		wLGnCDxPy8oExZXAx	,	3,7,2016, 6:09:00, PM
+		Zni6Soax4z3GW8Df2	,	3,7,2016, 6:45:00, PM
+		Q8rabz9pmuEnvSaoN	,	3,7,2016, 9:14:00, PM
+		XEFQSfMA4Np9cuPw7	,	3,8,2016, 12:52:00, AM
+		db7vBSrm7682SAdgu	,	3,8,2016, 2:39:00, AM
+		qTogdieYhMcvruSAz	,	3,8,2016, 9:34:00, AM
+		z5RvEc5BoDxBiH3iy	,	3,8,2016, 9:36:00, AM
+		kr7Pu2RZwAfTs2CKX	,	3,8,2016, 9:38:00, AM
+		KhryLZQ5TvoZre7Wq	,	3,8,2016, 9:42:00, AM
+		ZB5o3tfWNxeqjrk9A	,	3,8,2016, 9:57:00, AM
+		SPtAAc7NJbzx6HGnv	,	3,8,2016, 10:22:00, AM
+		RiaXcXZD8EMe3D675	,	3,8,2016, 10:27:00, AM
+		WWN5DFC5NTCBf5mzm	,	3,8,2016, 10:42:00, AM
+		cLfpHqBMQyBqJmqZN	,	3,8,2016, 10:42:00, AM
+		kErKZoNcgLDEQLh9h	,	3,8,2016, 10:59:00, AM
+		ANpFmJSXtcMZLsKb7	,	3,8,2016, 11:10:00, AM
+		RRGteZxa2sZLjy7tF	,	3,8,2016, 11:21:00, AM
+		MWawPm7rihwQdSfEc	,	3,8,2016, 11:50:00, AM
+		CpCwPvRPuXbcP6cz2	,	3,8,2016, 12:31:00, PM
+		wGaSKjvm4o49LhLrg	,	3,8,2016, 12:48:00, PM
+		4KFh6N3nNwuJAz9bM	,	3,8,2016, 3:06:00, PM
+		2EXM3D2Y33A7MB3o7	,	3,8,2016, 3:16:00, PM
+		xPohAATJY45H5Nmhy	,	3,8,2016, 4:10:00, PM
+		MLNEv85tZLCBBPkBJ	,	3,8,2016, 5:24:00, PM
+		yqvi6AycizdfMW67J	,	3,8,2016, 5:26:00, PM
+		GBR5Ti7uHcvswPGew	,	3,8,2016, 6:53:00, PM
+		MXzThn8PokRb4ock5	,	3,8,2016, 10:04:00, PM
+		2sTmttj6NkHQFsJLd	,	3,9,2016, 12:09:00, AM
+		smxtCGqQDdoxSBHnM	,	3,9,2016, 3:59:00, AM
+		xM423TEd4E3xpgssf	,	3,9,2016, 5:07:00, AM
+		JJ5EnHryZ8c2oGQCB	,	3,9,2016, 8:34:00, AM
+		n57DFjukqiJmSHEDQ	,	3,9,2016, 11:13:00, AM
+		DjhvjjCAx4ASj2o8R	,	3,9,2016, 5:21:00, PM
+		zsJhRyaRoAYMth497	,	3,9,2016, 7:02:00, PM
+		SJfYbZhnHTBay5yyX	,	3,9,2016, 8:39:00, PM
+		L9LpdeQFGzvFngGYS	,	3,9,2016, 9:28:00, PM
+		qifugRh7PhFJPpCmX	,	3,10,2016, 12:24:00, AM
+		cWth44wpGS4qP2Yub	,	3,10,2016, 1:16:00, AM
+		uHwSGkE8yothku3Pp	,	3,10,2016, 2:58:00, AM
+		cSZYtAZP8JYGj5YJm	,	3,10,2016, 9:40:00, AM
+		TRHpDyjtFJ4Bf98dL	,	3,10,2016, 9:49:00, AM
+		KgZMYh23LzaJQn9xQ	,	3,10,2016, 9:51:00, AM
+		7Rn59QWiRLMAcySTj	,	3,10,2016, 9:52:00, AM
+		vHTKf9kPuCEvY8Fb8	,	3,10,2016, 9:53:00, AM
+		XsvK4NT3Nh5wjR8En	,	3,10,2016, 9:56:00, AM
+		oWHGBei3dY4SjyLpw	,	3,10,2016, 9:57:00, AM
+		ZNHoLFfThXn3dxpiE	,	3,10,2016, 9:58:00, AM
+		NvQHkQfCNThsLfjfy	,	3,10,2016, 9:59:00, AM
+		E3NcHz7W573rHYeyY	,	3,10,2016, 10:03:00, AM
+		bWNtxPkGd8xNqDf2H	,	3,10,2016, 10:07:00, AM
+		8nBmn9tiXEPEpabJq	,	3,10,2016, 10:12:00, AM
+		Af6EwezJnoFReEpua	,	3,10,2016, 10:19:00, AM
+		BNHYtgF3y6oE5Z7KQ	,	3,10,2016, 10:21:00, AM
+		yYx4x55HrFLHFahMb	,	3,10,2016, 10:26:00, AM
+		5eAZsM6sSPgLp2AGc	,	3,10,2016, 10:33:00, AM
+		QzQ7qWvZvRxG3DTiQ	,	3,10,2016, 10:40:00, AM
+		MYDbh5c4b3ziMKmbt	,	3,10,2016, 10:44:00, AM
+		JzmhwhLyzer2RAe6v	,	3,10,2016, 10:46:00, AM
+		MA2uPdyzNp9BQAqer	,	3,10,2016, 10:47:00, AM
+		udy3MprwBQ5HiyMCT	,	3,10,2016, 10:52:00, AM
+		pJmeMQeWgG5t6W9qP	,	3,10,2016, 11:08:00, AM
+		QFG552t36L3EZH7Jy	,	3,10,2016, 11:19:00, AM
+		ShADd3RgtYRfwa2MZ	,	3,10,2016, 11:20:00, AM
+		jDE43c8DM6a8vfhLt	,	3,10,2016, 11:21:00, AM
+		LcANkd9aNJ5k5m36D	,	3,10,2016, 11:36:00, AM
+		btruDZRTdLHpSMw8Z	,	3,10,2016, 11:40:00, AM
+		k3GpWCr7PFT6C8hp7	,	3,10,2016, 11:42:00, AM
+		naXyBAkcHQnvmWAq2	,	3,10,2016, 11:46:00, AM
+		AQxTawn2raLNgqZ3P	,	3,10,2016, 11:55:00, AM
+		5myTGNQJvs2WKRHzh	,	3,10,2016, 12:13:00, PM
+		65rkkh9mvCWJswXdW	,	3,10,2016, 12:36:00, PM
+		DE4yEJwaXQHEzKcQa	,	3,10,2016, 12:44:00, PM
+		uiWHRDoF5BGb4wL2S	,	3,10,2016, 12:45:00, PM
+		gMminThzBtZJ7uSsx	,	3,10,2016, 12:53:00, PM
+		eCXK2ArfS3criAJTB	,	3,10,2016, 5:13:00, PM
+		qSx3DCWftYj2wsCyv	,	3,10,2016, 7:47:00, PM
+		e3qn8csAYTbHfMxkK	,	3,10,2016, 7:54:00, PM
+		46jJ5bQRXvenhFmTb	,	3,10,2016, 7:55:00, PM
+		e39vqG226v3BajR5r	,	3,10,2016, 7:56:00, PM
+		JYCXAYdtPkWscrdLB	,	3,10,2016, 7:59:00, PM
+		9ywHQR3mBPMwFxoz9	,	3,10,2016, 8:00:00, PM
+		47GQnCY9tKY6QcziM	,	3,10,2016, 8:00:00, PM
+		r7ouGiTYscdMz7S96	,	3,10,2016, 8:06:00, PM
+		q7T5qjCJmLKiFDM7B	,	3,10,2016, 8:07:00, PM
+		jfQvpyg84WrSgRNqv	,	3,10,2016, 8:08:00, PM
+		CBqTRZWRzXAgfN9Yy	,	3,10,2016, 8:09:00, PM
+		JSbt6gZfMo7joBzGv	,	3,10,2016, 8:13:00, PM
+		Rj9tu78F5w6w3X4H6	,	3,10,2016, 8:13:00, PM
+		Ny9gao85Xj9EsukBG	,	3,10,2016, 8:13:00, PM
+		yqM2BRFq8ZRfjjLkq	,	3,10,2016, 8:25:00, PM
+		sffrXtuXr5TiAB4mp	,	3,10,2016, 8:30:00, PM
+		EKWRiN76o86oB7anS	,	3,10,2016, 8:34:00, PM
+		WPYRr5aobwBj73DEK	,	3,10,2016, 8:52:00, PM
+		Qqh6muXhTuf3GrNiT	,	3,10,2016, 9:03:00, PM
+		PxTtMMdoKkhi9R3EA	,	3,10,2016, 9:10:00, PM
+		uaKgcq3eKvpoHhiRp	,	3,10,2016, 9:11:00, PM
+		P3KQ9YkjZxt4RLM8n	,	3,10,2016, 9:15:00, PM
+		2uka6dm2y2BiZJ2zv	,	3,10,2016, 9:16:00, PM
+		w7iHdtHuvS62tpX9A	,	3,10,2016, 9:17:00, PM
+		veJrsxDb3LXrES4xG	,	3,10,2016, 9:35:00, PM
+		yGMwpC8mFpdaBHsxc	,	3,10,2016, 9:35:00, PM
+		uSMjBeZWWWbr389eX	,	3,10,2016, 9:55:00, PM
+		L4ym7sk99zbs7SpRS	,	3,10,2016, 9:59:00, PM
+		tDQkgwFiLneeqykeP	,	3,10,2016, 10:30:00, PM
+		DJkv5Fv9S4kWNyB3h	,	3,10,2016, 10:43:00, PM
+		8LxJGcCiZ4Qc6jQuJ	,	3,10,2016, 11:13:00, PM
+		sd2kFccpmJrtKtWXt	,	3,10,2016, 11:32:00, PM
+		NfD5PKGzZSHiMziDZ	,	3,11,2016, 12:01:00, AM
+		sXZRJJZZ4ukn5Rhsa	,	3,11,2016, 12:21:00, AM
+		mhWeQdWa9FaKczAnW	,	3,11,2016, 12:01:00, PM
+		KtmBDbHyRWmxF3pSe	,	3,11,2016, 5:39:00, PM
+		C8ZZyaoAKK74u5sYq	,	3,11,2016, 8:25:00, PM
+		NEBKBc4v4z22JjHsM	,	3,11,2016, 9:59:00, PM
+		zPcauKCwjaC6ZF2to	,	3,12,2016, 2:02:00, AM
+		9et3SfHGXPybDEQvA	,	3,13,2016, 9:51:00, PM
+		dC92XDi5WvMMhu33h	,	3,15,2016, 3:09:00, AM
+		jFJJMNpjqg3TTB3vH	,	3,15,2016, 4:28:00, AM
+	
+	]
+
+positions = [0 for i in range(len(a))]
+
+def recursive_match(positions, a):
+    strings = map(lambda p, l: l[p], positions, a)
+    match = True
+    searched_string = strings.pop(0)[:4]
+    for string in strings:
+        if searched_string not in string:
+            match = False
+            break
+    if match:
+        match_IDs(searched_string)
+
